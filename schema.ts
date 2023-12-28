182853eb5f3daccd3fae0c8a31088da868b1ace1
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-12-28 10:54:23 +0300

    authentication

--- schema.ts
+++ schema.ts
@@ -9,7 +9,7 @@ export const schema = makeSchema({
     typegen: join(process.cwd(), "nexus-typegen.ts"),
   },
   contextType: {
-    module: join(process.cwd(), "./context.ts"),
+    module: join(process.cwd(), "./src/context.ts"),
     export: "Context",
   },
 });
